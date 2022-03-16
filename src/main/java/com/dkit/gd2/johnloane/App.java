9cd7f01f4a18d6cb3126f5aae531b3455cfa7d52
teomeo12 <d00240834@student.dkit.ie>
2022-03-16 12:35:29 +0000

    Question 1 a),b) Question 2 a) Question 4 a),b)

--- src/main/java/com/dkit/gd2/johnloane/App.java
+++ src/main/java/com/dkit/gd2/johnloane/App.java
@@ -1,41 +1,104 @@
 package com.dkit.gd2.johnloane;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * OOP CA3
- * @author John Loane
+ * @author Teodor Donchev
  */
 public class App 
 {
     public static void main( String[] args )
     {
+
+        ArrayList<Student> studentList = new ArrayList<>();
+
+        Student s1 = new Student(1,"David Jeferson",19);
+        Student s2 = new Student(2,"Tom Samson",18);
+        Student s3 = new Student(3,"John Armstrong",21);
+        Student s4 = new Student(4,"Steve Padelton",22);
+
+        studentList.add(s1);
+        studentList.add(s2);
+        studentList.add(s3);
+        studentList.add(s4);
         //Question 1
-        questionOnePartA();
-        questionOnePartB();
-        questionOnePartC();
-        questionOnePartD();
+        questionOnePartA(studentList);
+
+       // questionOnePartB(studentList);
+
+        //questionOnePartC(studentList);
+       // questionOnePartD();
 
         //Question 2
-        questionTwoPartA();
-        questionTwoPartB();
-        questionTwoPartC();
-        questionTwoPartD();
+        List<Employee> employeeList = new ArrayList<>();
+        Employee e1 = new Employee("John","Smith");
+        Employee e2 = new Employee("Andrea","Collon");
+        Employee e3 = new Employee("Derek","Riddle");
+        Employee e4 = new Employee("Cory","Smith");
+        Employee e5 = new Employee("Antoan","Champs");
+
+        employeeList.add(e1);
+        employeeList.add(e2);
+        employeeList.add(e3);
+        employeeList.add(e4);
+        employeeList.add(e5);
+
+        questionTwoPartA( employeeList);
+        pickLuckyWinner(employeeList);
+       // questionTwoPartB();
+       // questionTwoPartC();
+       // questionTwoPartD();
 
         //Question 3
         List<String> oneHundredStrings = new ArrayList<>();
         populateOneHundredStrings(oneHundredStrings);
-        questionThreePartA(oneHundredStrings);
-        questionThreePartB();
+       // questionThreePartA(oneHundredStrings);
+      //  questionThreePartB();
 
         //Question 4
         compareTwoStudents();
-        questionFourPartA();
+       questionFourPartA();
         questionFourPartB();
-        questionFourPartC();
+      //  questionFourPartC();
     }
+    public static void questionOnePartA(ArrayList<Student> studentList){
 
+        System.out.println("Student List");
+        System.out.println("__________________________________________");
+
+        for (Student student : studentList) {
+            System.out.println(student);
+        }
+
+    }
+    public static void questionOnePartB(ArrayList<Student> studentList){
+
+         Student[] studentArray = studentList.toArray(new Student[0]);
+
+         Arrays.sort(studentArray);
+         System.out.println(studentArray);
+
+        // ArrayList<Student> sortedstudentArray = new ArrayList<>(Arrays.asList(studentArray));
+    }
+    public static void questionOnePartC(ArrayList<Student> studentList){
+
+    }
+    public static void questionTwoPartA(List<Employee> employeeList) {
+        System.out.println("\nEmployee List");
+        System.out.println("__________________________________________");
+
+        for (Employee employee : employeeList) {
+            System.out.println(employee);
+        }
+    }
+    public static void  pickLuckyWinner(List<Employee> employeeList){
+         Random random = new Random();
+
+        int index = random.nextInt(employeeList.size());
+        System.out.println("\nThe winner is  :" +employeeList.get(index)  );
+
+    }
     //The method below relates to Question 4
     private static void compareTwoStudents()
     {
@@ -44,6 +107,12 @@ public class App
         System.out.println("alex1 hashcode = " + alex1.hashCode());
         System.out.println("alex2 hashcode = " + alex2.hashCode());
         System.out.println("Checking equality between alex1 and alex2 = " + alex1.equals(alex2));
+    }
+    public static void questionFourPartA(){
+        System.out.println("Because there no equals method implemented in the Student class");
+    }
+    public static void questionFourPartB(){
+        System.out.println("Because there no hashCode method implemented in the Student class");
     }
 
     //This is just a helper method to populate the list of strings for question 3. You do not need to read it to answer any of the questions in the CA
@@ -93,4 +162,5 @@ public class App
             }
         }
     }
+
 }
--- src/main/java/com/dkit/gd2/johnloane/Student.java
+++ src/main/java/com/dkit/gd2/johnloane/Student.java
@@ -1,16 +1,40 @@
 package com.dkit.gd2.johnloane;
 
+import java.util.Objects;
+
 public class Student
 {
     private int id;
     private String name;
     private int age;
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
     public Student(int id, String name, int age)
     {
         this.id = id;
         this.name = name;
         this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && age == student.age && name.equals(student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
     }
 
     public int getId()
